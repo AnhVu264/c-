d3af4191ea82691a4a99c0807a3f6291ee82adc9
AnhVu264 <124783782+AnhVu264@users.noreply.github.com>
2023-02-08 18:34:09 +0700

    Create c15.cpp

---
+++ c15.cpp
@@ -0,0 +1,17 @@
+c15: #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string f0 = "a", f1 = "b", fn = "ba";
+    cout << f0 << " " << f1 << " " << fn;
+    for (int i = 3; i <= 10; i++)
+    {
+        f0 = f1;
+        f1 = fn;
+        fn = f1 + f0;
+        cout << " " << fn;
+    }
+    return 0;
+}
