ad9fa3d6c9082d74e16c6a059b2f83accdaffe01
AnhVu264 <124783782+AnhVu264@users.noreply.github.com>
2023-02-10 21:47:16 +0700

    Create c18.cpp

---
+++ c18.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x1, x2, y1, y2;
+    cin >> x1 >> x2 >> y1 >> y2;
+    cout << sqrt((y1-x1) * (y1-x1) + (y2 - x2) * (y2 - x2));
+
+    return 0;
+}
