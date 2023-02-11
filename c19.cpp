d61ffee26d19c1e68c1ff873295fda342cd98ece
AnhVu264 <124783782+AnhVu264@users.noreply.github.com>
2023-02-11 10:28:24 +0700

    Create c19.cpp

---
+++ c19.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool b = false;
+    int x, y, z;
+    cin >> x >> y >> z;
+    if (x < y && y < z)
+    {
+        b = true;
+    }
+    else if (x > y && y > z)
+    {
+        b = true;
+    }
+    cout << b;
+
+    return 0;
+}
